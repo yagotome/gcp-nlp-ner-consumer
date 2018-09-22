fc9a179c65c414d2157535a5f4b322e908bdfa38
Yago Tomé <yago.tome@hotmail.com>
2018-09-22 14:12:16 -0300

    Finish createtsv script

--- cmd/createtsv/main.go
+++ cmd/createtsv/main.go
@@ -12,13 +12,24 @@ import (
 	"github.com/euskadi31/go-tokenizer"
 	"github.com/yagotome/gcp-nlp-ner-consumer/csv"
 	"github.com/yagotome/gcp-nlp-ner-consumer/nlp"
+	"github.com/yagotome/gcp-nlp-ner-consumer/utils/stringutil"
+)
+
+var (
+	progress        = 0
+	sentencesAmount = 0
 )
 
 func main() {
 	log.Println("Starting processing...")
-	groupedSentences := getGroupedSentences()
+	gss := getGroupedSentences()
 
-	groupedSentences = map[string][]string{"Roubos em Geral": []string{"Roubaram meu carro. Na Barra da Tijuca, em frente ao Barra Shopping! Incidente ocorreu hoje de manhã, 10:30."}}
+	groupedSentences := map[string][]string{}
+	for k, v := range gss {
+		// groupedSentences[k] = v[:2]
+		groupedSentences[k] = v
+		sentencesAmount += len(groupedSentences[k])
+	}
 
 	for group, sentences := range groupedSentences {
 		createTsv(group, sentences)
@@ -31,18 +42,31 @@ func createTsv(group string, sentences []string) {
 	wg.Add(len(sentences))
 	mutex := sync.Mutex{}
 
-	f, err := os.Create(fmt.Sprintf("output/%v.tsv", group))
+	f, err := os.Create(fmt.Sprintf("output/%v.tsv", stringutil.EscapeGroup(group)))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
+	ef, err := os.Create(fmt.Sprintf("error/%v.tsv", stringutil.EscapeGroup(group)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ef.Close()
+	ef2, err := os.Create(fmt.Sprintf("error/%v.csv", stringutil.EscapeGroup(group)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ef2.WriteString("assunto,relato\n")
+	defer ef2.Close()
 
 	for _, text := range sentences {
 		go func(text string) {
 			defer wg.Done()
 			entities, err := nlp.GetNamedEntities(text)
+			// err = fmt.Errorf("error fake")
 			if err != nil {
-				log.Println(err)
+				log.Println("Error on sentence:", text+"\n", err)
+				ef2.WriteString(fmt.Sprintf("%v,%v\n", group, `"`+strings.Replace(text, `"`, `""`, -1)+`"`))
 			}
 			// cleanText := stringutil.RemovePunctuation(text)
 			// phrases := stringutil.SplitByPunctuation(cleanText)
@@ -60,7 +84,9 @@ func createTsv(group string, sentences []string) {
 					if r.MatchString(words[i]) {
 						continue
 					}
-					if entity := entities.Match(words, i); entity != nil {
+					if err != nil {
+						ef.WriteString(words[i] + "\t" + "O" + "\n")
+					} else if entity := entities.Match(words, i); entity != nil {
 						f.WriteString(entity.Name + "\t" + entity.Type + "\n")
 						entityWords := strings.Split(entity.Name, " ")
 						i += len(entityWords) - 1
@@ -68,19 +94,25 @@ func createTsv(group string, sentences []string) {
 						f.WriteString(words[i] + "\t" + "O" + "\n")
 					}
 				}
-				f.WriteString("\n")
+				if err != nil {
+					ef.WriteString("\n")
+				} else {
+					f.WriteString("\n")
+				}
 			}
+			progress++
+			log.Printf("%.2f%% - %d de %d", 100*float64(progress)/float64(sentencesAmount), progress, sentencesAmount)
 			mutex.Unlock()
 		}(text)
-		time.Sleep(time.Millisecond)
+		time.Sleep(150 * time.Millisecond)
 	}
-
 	wg.Wait()
 }
 
 func getGroupedSentences() map[string][]string {
 	subjectFilter := []string{"Roubos em Geral", "Roubo carga/ veículo", "Tráfico de drogas", `Tráfico de Drogas \ Armas`, "Homicidio", "Homicídios", "Armas"}
 	groupedSentences, err := csv.ExtractColumnGroupedBy("app_dd.csv", "relato", "assunto", subjectFilter)
+	// groupedSentences, err := csv.ExtractColumnGroupedBy("error/Tráfico de drogas.csv", "relato", "assunto", subjectFilter)
 	if err != nil {
 		log.Fatal(err)
 	}
--- utils/stringutil/stringutil_test.go
+++ utils/stringutil/stringutil_test.go
@@ -11,3 +11,27 @@ func TestSplitByPunctuation(t *testing.T) {
 	actual := SplitByPunctuation("Foobar. É um cara bem legal!!! Pena que...\t não pode ver mulher!?\nela ta dançando.")
 	assert.ElementsMatch(t, expected, actual)
 }
+
+func TestEscapeGroup(t *testing.T) {
+	type args struct {
+		group string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Single slash", args{"P/D"}, "P-D"},
+		{"Single inverted slash", args{"P\\D"}, "P-D"},
+		{"Multiple slashes", args{"A / / B/ D"}, "A - - B- D"},
+		{"Multiple inverted slashes", args{"A\\B \\ C \\"}, "A-B - C -"},
+		{"Mixed", args{"\\A/B \\ C/D"}, "-A-B - C-D"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := EscapeGroup(tt.args.group); got != tt.want {
+				t.Errorf("EscapeGroup() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
